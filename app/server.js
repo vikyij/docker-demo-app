b366a0bfc169a218c73d4ed87a9fc2876218a331
vikyij <vikkyij54@gmail.com>
2025-02-11 13:56:57 +0100

    add docker volumes

--- app/server.js
+++ app/server.js
@@ -2,13 +2,8 @@ let express = require("express");
 let path = require("path");
 let fs = require("fs");
 let MongoClient = require("mongodb").MongoClient;
-// let bodyParser = require('body-parser');
 let app = express();
 
-// app.use(bodyParser.urlencoded({
-//   extended: true
-// }));
-// app.use(bodyParser.json());
 
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
@@ -21,10 +16,10 @@ app.get("/profile-picture", function (req, res) {
 });
 
 // use when starting application locally
-// let mongoUrlLocal = "mongodb://admin:password@localhost:27017";
+let mongoUrlLocal = "mongodb://admin:password@localhost:27017";
 
 // // use when starting application as docker container
-let mongoUrlDocker = "mongodb://admin:password@mongo:27017";
+// let mongoUrlDocker = "mongodb://admin:password@mongo:27017";
 
 // // pass these options to mongo client connect request to avoid DeprecationWarning for current Server Discovery and Monitoring engine
 let mongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
@@ -37,7 +32,7 @@ app.use(express.json());
 app.post("/update-profile", function (req, res) {
   let userObj = req.body;
   MongoClient.connect(
-    mongoUrlDocker,
+    mongoUrlLocal,
     mongoClientOptions,
     function (err, client) {
       if (err) throw err;
@@ -67,7 +62,7 @@ app.get("/get-profile", function (req, res) {
   let response = {};
   // Connect to the db
   MongoClient.connect(
-    mongoUrlDocker,
+    mongoUrlLocal,
     mongoClientOptions,
     function (err, client) {
       if (err) throw err;
